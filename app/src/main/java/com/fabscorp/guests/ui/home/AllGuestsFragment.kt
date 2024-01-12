d7fbe3fcea2678328942b5c0c4a513200ecca8eb
Fabricio Trindade <fabrtrindade@gmail.com>
2024-01-12 09:16:47 -0300

    Editing HomeFragment for the application

--- app/src/main/java/com/fabscorp/guests/ui/home/AllGuestsFragment.kt
+++ app/src/main/java/com/fabscorp/guests/ui/home/AllGuestsFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.fabscorp.guests.databinding.FragmentHomeBinding
+import com.fabscorp.guests.databinding.FragmentAllGuestsBinding
 
-class HomeFragment : Fragment() {
+class AllGuestsFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentAllGuestsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val ViewModel =
+            ViewModelProvider(this).get(AllGuestViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentAllGuestsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
+        ViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
