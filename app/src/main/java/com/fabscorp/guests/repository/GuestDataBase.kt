1c581e88070a46981af8079ce3b27dca486512a2
Fabricio Trindade <fabrtrindade@gmail.com>
2024-01-24 08:37:49 -0300

    Firsts steps to create SQLite DB

---
+++ app/src/main/java/com/fabscorp/guests/repository/GuestDataBase.kt
@@ -0,0 +1,21 @@
+package com.fabscorp.guests.repository
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class GuestDataBase(context: Context,) : SQLiteOpenHelper(context, NAME, null, VERSION) {
+
+    companion object {
+        private const val NAME = "guestdb"
+        private const val VERSION = 1
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        // Data Base Create
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+}
