852909dfeef7854c72ead0b2e026336f9a55f6f6
Fabricio Trindade <fabrtrindade@gmail.com>
2024-01-30 09:54:27 -0300

    Implementing a DB insert function

--- app/src/main/java/com/fabscorp/guests/model/GuestModel.kt
+++ app/src/main/java/com/fabscorp/guests/model/GuestModel.kt
@@ -1,4 +1,4 @@
-package com.fabscorp.guests
+package com.fabscorp.guests.model
 
 data class GuestModel (val id: Int, var name: String, var presence: Boolean){
 }
--- app/src/main/java/com/fabscorp/guests/repository/GuestRepository.kt
+++ app/src/main/java/com/fabscorp/guests/repository/GuestRepository.kt
@@ -1,6 +1,8 @@
 package com.fabscorp.guests.repository
 
+import android.content.ContentValues
 import android.content.Context
+import com.fabscorp.guests.model.GuestModel
 
 class GuestRepository private constructor(context: Context){
 
@@ -16,5 +18,19 @@ class GuestRepository private constructor(context: Context){
             }
             return repository
         }
+    }
+
+    fun insert (guest: GuestModel){
+        val db = guestDataBase.writableDatabase
+
+        val presence = if (guest.presence)  1 else 0
+
+        val values = ContentValues()
+
+        values.put("name", guest.name)
+        values.put("presence", presence)
+
+        db.insert("Guest", null, values)
+
     }
 }
