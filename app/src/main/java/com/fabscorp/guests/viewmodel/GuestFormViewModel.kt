3ded0ab8ad03fb68e3aac78898fc0620412241dd
Fabricio Trindade <fabrtrindade@gmail.com>
2024-01-29 22:30:12 -0300

    Passing a context through a ViewModel

--- app/src/main/java/com/fabscorp/guests/viewmodel/GuestFormViewModel.kt
+++ app/src/main/java/com/fabscorp/guests/viewmodel/GuestFormViewModel.kt
@@ -1,8 +1,10 @@
 package com.fabscorp.guests.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import com.fabscorp.guests.repository.GuestRepository
 
-class GuestFormViewModel: ViewModel(){
-    private val repository = GuestRepository.getInstance()
+class GuestFormViewModel(application: Application) : AndroidViewModel(application){
+    private val repository = GuestRepository.getInstance(application)
 }
