fcce54f1a878bb816429eeb16401c48a31063c3b
Fabricio Trindade <fabrtrindade@gmail.com>
2024-04-16 18:51:51 -0300

    RecyclerView: Creating a guest delete alert

--- app/src/main/java/com/fabscorp/guests/view/viewholder/GuestViewHolder.kt
+++ app/src/main/java/com/fabscorp/guests/view/viewholder/GuestViewHolder.kt
@@ -1,8 +1,12 @@
 package com.fabscorp.guests.view.viewholder
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.view.View
 import android.view.View.OnClickListener
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.fabscorp.guests.R
 import com.fabscorp.guests.databinding.RowGuestBinding
 import com.fabscorp.guests.model.GuestModel
 import com.fabscorp.guests.view.listener.OnGuestListener
@@ -18,7 +22,28 @@ class GuestViewHolder(private val bind: RowGuestBinding, private val listener: O
         }
 
         bind.textMame.setOnLongClickListener {
-            listener.onDelete(guest.id)
+
+            AlertDialog.Builder(itemView.context)
+                .setTitle(itemView.context.getString(R.string.guest_remove_alert))
+                .setMessage(itemView.context.getString(R.string.guest_remove))
+                .setNegativeButton(
+                    itemView.context.getString(R.string.no)
+                ) { dialog, wich -> //example with lambda
+                    Toast.makeText(
+                        itemView.context,
+                        itemView.context.getString(R.string.op_cancelled),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                .setPositiveButton(
+                    itemView.context.getString(R.string.yes),
+                    object : DialogInterface.OnClickListener { //example without lambda
+                        override fun onClick(dialog: DialogInterface?, wich: Int) {
+                            listener.onDelete(guest.id)
+                        }
+                    })
+                .create()
+                .show()
             true
         }
 
