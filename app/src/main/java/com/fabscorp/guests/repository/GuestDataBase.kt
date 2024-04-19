c5501cb06b102fe1aef12f9e470dc20e3bc13eec
Fabricio Trindade <fabrtrindade@gmail.com>
2024-04-19 13:25:29 -0300

    Room: Adapting database initialization for Room use

--- app/src/main/java/com/fabscorp/guests/repository/GuestDataBase.kt
+++ app/src/main/java/com/fabscorp/guests/repository/GuestDataBase.kt
@@ -3,24 +3,40 @@ package com.fabscorp.guests.repository
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.fabscorp.guests.constants.DataBaseConstants
+import com.fabscorp.guests.model.GuestModel
 
-class GuestDataBase(context: Context,) : SQLiteOpenHelper(context, NAME, null, VERSION) {
+@Database (entities = [GuestModel::class], version = 1)
+abstract class GuestDataBase : RoomDatabase() {
 
+    //Singleton
     companion object {
-        private const val NAME = "guestdb"
-        private const val VERSION = 1
-    }
+        private lateinit var INSTANCE: GuestDataBase
+        fun getDataBase(context: Context): GuestDataBase {
+            synchronized(GuestDataBase::class.java) {// To prevent two or more threads from executing
+                                                     //the same section below
+                if (!::INSTANCE.isInitialized) {
+                    INSTANCE =
+                        Room.databaseBuilder(context, GuestDataBase::class.java, "guestdb")
+                            .addMigrations(MIGRATION_1_2)
+                            .allowMainThreadQueries()
+                            .build()
+                }
+                return INSTANCE
 
-    override fun onCreate(db: SQLiteDatabase) {
-        // Data Base Create
-        db.execSQL("CREATE TABLE" + DataBaseConstants.GUEST.TABLE_NAME + "(" +
-                DataBaseConstants.GUEST.COLUMNS.ID + " integer primary key autoincrement," +
-                DataBaseConstants.GUEST.COLUMNS.NAME + " text," +
-                DataBaseConstants.GUEST.COLUMNS.PRESENCE +" integer);")
-    }
+            }
+        }
 
-    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        TODO("Not yet implemented")
+        private val MIGRATION_1_2: Migration = object: Migration (1, 2){
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL("DELETE FROM Guest")
+            }
+
+        }
     }
 }
